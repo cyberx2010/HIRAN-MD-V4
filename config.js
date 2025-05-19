6db470aa43945108e776a6a2012c18e7a6ae3296
cyberx2010 <cyberxbot3@gmail.com>
2025-05-19 22:39:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7BMmmLrB#5YJCZNG0-a6BEUga_Vqmz8U3UNcbpAbRHZAj-SB-xl4"
+SESSION_ID: process.env.SESSION_ID || "7BMmmLrB#5YJCZNG0-a6BEUga_Vqmz8U3UNcbpAbRHZAj-SB-xl4",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/nMSm7kCD/file-000000002fb461f79161fc4b64d5b0ff.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
