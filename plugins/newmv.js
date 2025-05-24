5ce613dbc9be5394209ac9505a14da1c977d0ba0
cyberx2010 <cyberxbot3@gmail.com>
2025-05-24 11:57:17 +0530

    Update newmv.js

--- plugins/newmv.js
+++ plugins/newmv.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const { cmd, commands } = require('../lib/command')
+const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
 const axios = require('axios');
 const cheerio = require('cheerio');
